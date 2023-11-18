dad2306652d7a887d44831bcd65b6c8770e71a1f
tangjm <tangjm.dev@outlook.com>
2023-11-18 17:28:25 +0000

    We serve from "./dist" in development so we want relative paths to our css files

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js")
 
+common.output.publicPath = ""; 
+
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
